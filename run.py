74fa3eca5b2c744175648b23eb826026931137dc
Brioni Connolly <brioniconnolly@gmail.com>
2023-07-08 14:45:08 +0000

    initial commit

--- run.py
+++ run.py
@@ -1,3 +1,21 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+#1234567891123456789212345678931234567894123456789512345678961234567897123456789
+
+def start_game():
+    print(               _____)
+    print(            ,-"     "-.)
+    print(           / o       o \)
+    print(          /   \     /   \)
+    print(         /     )-"-(     \)
+    print(        /     ( 6 6 )     \)
+    print(       /       \ " /       \)
+    print(      /         )=(         \)
+    print(     /   o   .--"-"--.   o   \)
+    print(    /    I  /  -   -  \  I    \)
+    print(.--(    (_}y/\       /\y{_)    )--.)
+    print(    ".___l\/__\_____/__\/l___,"    )
+    print(\                                 /)
+    print( "-._      o O o O o O o      _,-")
+    print(     `--Y--.___________.--Y--')
+    print(        |==.___________.==| hjw)
+    print(        `==.___________.==' `97)
